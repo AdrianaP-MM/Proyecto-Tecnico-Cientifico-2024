50f9e2597a7c9c3b7e3216f8cfabcbb0bdb521fa
Jhirdand <133984359+Jhirdand@users.noreply.github.com>
2024-07-04 22:20:28 -0600

    Servicios Detalles servicios y Cards de panel Principal

---
+++ api/models/data/servicio_data.php
@@ -0,0 +1,74 @@
+<?php
+// Se incluye la clase para validar los datos de entrada.
+require_once('../../helpers/validator.php');
+// Se incluye la clase para manejar los datos de la tabla tb_servicios.
+require_once('../../models/handler/servicio_handler.php');
+
+/*
+ * Clase para manejar el encapsulamiento de los datos de la tabla tb_servicios.
+ */
+class ServiciosData extends ServicioHandler
+{
+    private $data_error = null;
+
+    // Método para establecer el ID del servicio
+    public function setIdServicio($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->id_servicio = $value;
+            return true;
+        } else {
+            $this->data_error = 'El identificador del servicio es incorrecto';
+            return false;
+        }
+    }
+
+    // Método para establecer el ID del tipo de servicio
+    public function setIdTipoServicio($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->id_tipo_servicio = $value;
+            return true;
+        } else {
+            $this->data_error = 'El identificador del tipo de servicio es incorrecto';
+            return false;
+        }
+    }
+
+    // Método para establecer el nombre del servicio
+    public function setNombreServicio($value, $min = 1, $max = 50)
+    {
+        if (!Validator::validateAlphanumeric($value)) {
+            $this->data_error = 'El nombre del servicio debe ser alfanumérico';
+            return false;
+        } elseif (!Validator::validateLength($value, $min, $max)) {
+            $this->data_error = 'El nombre del servicio debe tener una longitud entre ' . $min . ' y ' . $max;
+            return false;
+        } else {
+            $this->nombre_servicio = $value;
+            return true;
+        }
+    }
+
+    // Método para establecer la descripción del servicio
+    public function setDescripcionServicio($value, $min = 1, $max = 255)
+    {
+        if (!Validator::validateAlphabetic($value)) {
+            $this->data_error = 'La descripción del servicio debe ser un texto válido';
+            return false;
+        } elseif (!Validator::validateLength($value, $min, $max)) {
+            $this->data_error = 'La descripción del servicio debe tener una longitud entre ' . $min . ' y ' . $max;
+            return false;
+        } else {
+            $this->descripcion_servicio = $value;
+            return true;
+        }
+    }
+
+    // Método para obtener el error de los datos
+    public function getDataError()
+    {
+        return $this->data_error;
+    }
+}
+?>
---
+++ api/services/privado/servicio.php
@@ -0,0 +1,92 @@
+<?php
+require_once ('../../helpers/validator.php');
+require_once ('../../models/data/servicio_data.php');
+
+if (isset($_GET['action'])) {
+    session_start();
+    $servicioData = new ServiciosData;
+    $result = array('status' => 0, 'session' => 0, 'message' => null, 'dataset' => null, 'error' => null, 'exception' => null, 'username' => null);
+
+    if (isset($_SESSION['idAdministrador'])) {
+        $result['session'] = 1;
+        switch ($_GET['action']) {
+            case 'readAll':
+                if ($result['dataset'] = $servicioData->readAll()) {
+                    $result['status'] = 1;
+                } else {
+                    $result['error'] = 'No existen servicios para mostrar';
+                }
+                break;
+            case 'readOne':
+                if (!isset($_POST['id_tipo_servicio']) || !$servicioData->setIdTipoServicio($_POST['id_tipo_servicio'])) {
+                    $result['error'] = $servicioData->getDataError();
+                } elseif ($result['dataset'] = $servicioData->readOne()) {
+                    $result['status'] = 1;
+                } else {
+                    $result['error'] = 'No existen servicios para mostrar';
+                }
+                break;
+            case 'readOneModal':
+                if (!$servicioData->setIdServicio($_POST['id_servicio'])) {
+                    $result['error'] = $servicioData->getDataError();
+                } elseif ($result['dataset'] = $servicioData->readOneModal()) {
+                    $result['status'] = 1;
+                } else {
+                    $result['error'] = 'No existen servicios para mostrar';
+                }
+                break;
+            case 'createRow':
+                $_POST = Validator::validateForm($_POST);
+                //print_r($_POST);
+                if (
+                    !$servicioData->setIdTipoServicio($_POST['id_tipo_servicio']) ||
+                    !$servicioData->setNombreServicio($_POST['nombre_servicio']) ||
+                    !$servicioData->setDescripcionServicio($_POST['descripcion_servicio'])
+                ) {
+                    $result['error'] = $servicioData->getDataError();
+                } elseif ($servicioData->createRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Servicio creado correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al crear el servicio';
+                }
+                break;
+            case 'updateRow':
+                $_POST = Validator::validateForm($_POST);
+                if (
+                    !$servicioData->setIdServicio($_POST['id_servicio']) ||
+                    !$servicioData->setIdTipoServicio($_POST['id_tipo_servicio']) ||
+                    !$servicioData->setNombreServicio($_POST['nombre_servicio']) ||
+                    !$servicioData->setDescripcionServicio($_POST['descripcion_servicio'])
+                ) {
+                    $result['error'] = $servicioData->getDataError();
+                } elseif ($servicioData->updateRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Servicio modificado correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al modificar el servicio';
+                }
+                break;
+            case 'deleteRow':
+                if (!isset($_POST['id_servicio']) || !$servicioData->setIdServicio($_POST['id_servicio'])) {
+                    $result['error'] = 'ID del servicio incorrecto';
+                } elseif ($servicioData->deleteRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Servicio eliminado correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al eliminar el servicio';
+                }
+                break;
+            default:
+                $result['error'] = 'Acción no disponible dentro de la sesión';
+        }
+    } else {
+        $result['exception'] = 'Acceso denegado';
+    }
+
+    header('content-type: application/json; charset=utf-8');
+    print (json_encode($result));
+} else {
+    print (json_encode('Recurso no disponible'));
+}
+?>
