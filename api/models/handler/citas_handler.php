ccd49c8e33d403f2d792d41c4b5721a136017ad9
AdrianaP-MM <20190015@ricaldone.edu.sv>
2024-09-17 21:42:49 -0600

    Arreglos en citas handler

--- api/models/handler/citas_handler.php
+++ api/models/handler/citas_handler.php
@@ -70,11 +70,45 @@ class CitasHandler
 
     public function searchByFechaLLegada()
     {
-        $sql = 'SELECT * FROM tb_citas;';
+        $sql = 'SELECT c.*, a.*, cl.*,
+                CONCAT(
+                CASE DAYOFWEEK(c.fecha_hora_cita)
+                    WHEN 1 THEN "Domingo"
+                    WHEN 2 THEN "Lunes"
+                    WHEN 3 THEN "Martes"
+                    WHEN 4 THEN "Miércoles"
+                    WHEN 5 THEN "Jueves"
+                    WHEN 6 THEN "Viernes"
+                    WHEN 7 THEN "Sábado"
+                END, 
+                " ", DATE_FORMAT(c.fecha_hora_cita, "%d de "),
+                CASE MONTH(c.fecha_hora_cita)
+                    WHEN 1 THEN "Enero"
+                    WHEN 2 THEN "Febrero"
+                    WHEN 3 THEN "Marzo"
+                    WHEN 4 THEN "Abril"
+                    WHEN 5 THEN "Mayo"
+                    WHEN 6 THEN "Junio"
+                    WHEN 7 THEN "Julio"
+                    WHEN 8 THEN "Agosto"
+                    WHEN 9 THEN "Septiembre"
+                    WHEN 10 THEN "Octubre"
+                    WHEN 11 THEN "Noviembre"
+                    WHEN 12 THEN "Diciembre"
+                END
+                ) AS fecha_cita,
+                DATE_FORMAT(c.fecha_hora_cita, "%l:%i %p") AS hora_cita,
+                DATE_FORMAT(c.fecha_hora_cita, "%Y") AS anio_cita
+                FROM tb_citas c
+                INNER JOIN tb_automoviles a ON c.id_automovil = a.id_automovil
+                INNER JOIN tb_clientes cl ON a.id_cliente = cl.id_cliente
+                WHERE cl.id_cliente = ? AND c.estado_cita = ? AND DATE(c.fecha_hora_cita) = ?';
         $params = array(
-            $this->id_automovil
+            $_SESSION['idUsuarioCliente'],
+            $this->estado_cita,
+            $this->fecha_hora_cita
         );
-        return Database::getRow($sql, $params);
+        return Database::getRows($sql, $params);
     }
 
     public function getDemandaServicioMensual()
