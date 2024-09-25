56eee6ea69030b948ec856509f43ac7af9eee968
AdrianaP-MM <20190015@ricaldone.edu.sv>
2024-09-24 18:04:17 -0600

    Diseños implementados error 404 y error 403

--- controlador/privado/error_403.js
+++ controlador/privado/error_403.js
@@ -1,3 +1,3 @@
-document.addEventListener('DOMContentLoaded', async () => {
-
-});
+function gotoPanel(){
+    location.href = `/proyecto-tecnico-cientifico-2024/vistas/privado/panel_principal.html`;
+}
